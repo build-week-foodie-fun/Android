4131db13b6e019bbecb2368c0816d1cb8ac3259a
alihaider334 <ali334.haider@gmail.com>
2019-08-26 21:59:24 -0400

    App Launcher Page & FoodEntry Data Class Created

---
+++ app/src/main/java/com/example/foodiefun/FoodEntry.kt
@@ -0,0 +1,19 @@
+package com.example.foodiefun
+
+import android.net.Uri
+
+class FoodEntry(id:Int,restaurant_name:String,restaurant_type:String,item_name:
+String,photoUri:Uri,food_rating:Int,comments:String,price:Double,wait_time:String,date_of_visit:String){
+
+    val id=id
+    var restaurant_name=restaurant_name
+    var restaurant_type=restaurant_type
+    var item_name=item_name
+    val photoUri=photoUri
+    var food_rating=food_rating
+    var comments=comments
+    var price=price
+    var wait_time=wait_time
+    var date_of_visit=date_of_visit
+
+}
