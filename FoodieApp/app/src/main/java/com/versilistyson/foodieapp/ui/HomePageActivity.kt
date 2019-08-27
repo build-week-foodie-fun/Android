096c2ee6555398475be5af3dbdbd771adf951c34
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-27 19:23:40 -0400

    Adds listener for add_review_fab to request an image and pass it to another activity to create a view

--- FoodieApp/app/src/main/java/com/versilistyson/foodieapp/ui/HomePageActivity.kt
+++ FoodieApp/app/src/main/java/com/versilistyson/foodieapp/ui/HomePageActivity.kt
@@ -1,13 +1,32 @@
 package com.versilistyson.foodieapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.versilistyson.foodieapp.R
+import kotlinx.android.synthetic.main.activity_homepage.*
 
 class HomePageActivity : AppCompatActivity() {
+
+    companion object {
+        const val imageRequestCode = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        add_review_fab.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), imageRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == imageRequestCode) {
+            //TODO: Open up activity or fragment for adding in data
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
