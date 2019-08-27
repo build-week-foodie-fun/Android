8abe7512dbb56e3718111c87b542b63dfbec05d7
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-27 18:50:32 -0400

    Adds ReviewItem class with parameters for adding food reviews.

--- FoodieApp/app/src/main/java/com/versilistyson/foodieapp/ReviewListFragment.kt
+++ FoodieApp/app/src/main/java/com/versilistyson/foodieapp/ReviewListFragment.kt
@@ -10,7 +10,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.versilistyson.foodieapp.dummy.DummyContent
 import com.versilistyson.foodieapp.dummy.DummyContent.DummyItem
 
 /**
