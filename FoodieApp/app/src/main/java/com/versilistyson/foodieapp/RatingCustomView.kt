5a9823096d71d159b55437f6d5ca2e953a3262f0
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-28 15:32:14 -0400

    Creates assets folder and moves fonts

--- FoodieApp/app/src/main/java/com/versilistyson/foodieapp/RatingCustomView.kt
+++ FoodieApp/app/src/main/java/com/versilistyson/foodieapp/RatingCustomView.kt
@@ -1,4 +1,12 @@
 package com.versilistyson.foodieapp
 
-class RatingCustomView {
+import android.util.AttributeSet
+import android.view.View
+
+class RatingCustomView : View {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributesSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
 }
