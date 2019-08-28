871b2f12b2996e975e694318c1829a646d22826a
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-28 15:46:16 -0400

    Creates custom view

--- FoodieApp/app/src/main/java/com/versilistyson/foodieapp/RatingCustomView.kt
+++ FoodieApp/app/src/main/java/com/versilistyson/foodieapp/RatingCustomView.kt
@@ -1,12 +1,17 @@
 package com.versilistyson.foodieapp
 
+import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 
 class RatingCustomView : View {
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context, attrs: AttributesSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
 
+    init {
+        val foodRatingBar = LayoutInflater.from(context).inflate(R.layout.custom_rating_bar_layout)
     }
 }
