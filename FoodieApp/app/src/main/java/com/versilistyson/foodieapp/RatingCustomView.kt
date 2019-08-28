92cb6b630678fa899866b0479eaf41052bf9953b
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-28 16:05:52 -0400

    Inflates custom rating bar and rating text view in Rating custom view

--- FoodieApp/app/src/main/java/com/versilistyson/foodieapp/RatingCustomView.kt
+++ FoodieApp/app/src/main/java/com/versilistyson/foodieapp/RatingCustomView.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.LinearLayout.VERTICAL
+import android.widget.RatingBar
+import android.widget.TextView
 
 class RatingCustomView : View {
     constructor(context: Context): super(context)
@@ -12,6 +16,12 @@ class RatingCustomView : View {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
 
     init {
-        val foodRatingBar = LayoutInflater.from(context).inflate(R.layout.custom_rating_bar_layout)
+        val mLinearLayout = LinearLayout(context)
+        val foodRatingBar = LayoutInflater.from(context).inflate(R.layout.custom_rating_bar_layout,mLinearLayout , false) as RatingBar
+        val ratingText = LayoutInflater.from(context).inflate(R.layout.food_ratingbar_textview, mLinearLayout, false) as TextView
+
+        mLinearLayout.orientation = VERTICAL
+        mLinearLayout.addView(foodRatingBar)
+        mLinearLayout.addView(ratingText)
     }
 }
